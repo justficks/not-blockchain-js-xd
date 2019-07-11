75ab7eb4a5e7ec280d0131b61529371fea2a1ba2
justficks <firsoff091@gmail.com>
2019-07-11 11:37:01 +0700

    Вывод главной страницы

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var app = require('express')();
-var http = require('http').Server(app);
+const app = require('express')();
+const http = require('http').Server(app);
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
 
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'layout' }));
+app.set('view engine', 'handlebars');
+
+app.use('/', require('./routes/index'));
 
 http.listen(5000, () => console.log('Server started listening on port 5000!'));
